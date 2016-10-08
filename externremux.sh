a8cb2d719b1af0c1f5df36dcc8d936f1d408420f
Gerald Dachs <gda@dachsweb.de>
2016-10-08 21:31:01 +0200

    first version that runs
    fix typo
    
    Update README.md
    
    Fixed formatting

---
+++ externremux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+case "$REMUX_VPID" in
+''|0|1) CONTENTTYPE='audio/mpeg';;
+*) CONTENTTYPE='video/mpeg';;
+esac
+
+if [ "$SERVER_PROTOCOL" = HTTP ]; then
+  echo -ne "Content-type: ${CONTENTTYPE}\r\n"
+  echo -ne '\r\n'
+  # abort after headers
+  [ "$REQUEST_METHOD" = HEAD ] && exit 0
+fi
+
+ffmpeg -y -i - -dn -ignore_unknown -c copy -map 0 -f mpegts pipe:1 2>/tmp/externremux.$$
